58c356e130189cbdc9e7acbcdf3dd05e2b45535d
roman-haidarov <romnhajdarov@gmail.com>
2022-07-09 12:08:36 +0500

    added action create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,22 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new()
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
