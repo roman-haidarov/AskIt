5ec173d092449cc6b7701123628c55fa3ea5fcb1
roman-haidarov <romnhajdarov@gmail.com>
2022-07-10 11:10:09 +0500

    update form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:edit, :update]
+
   def index
     @questions = Question.all
   end
@@ -17,7 +19,22 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @question.update question_params
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_question
+    @question = Question.find_by(id: params[:id])
+  end
 
   def question_params
     params.require(:question).permit(:title, :body)
