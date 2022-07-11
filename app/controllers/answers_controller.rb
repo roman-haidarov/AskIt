0285c7f58c492533d29cf4610c12db2acb83e25a
roman-haidarov <romnhajdarov@gmail.com>
2022-07-11 21:32:47 +0500

    added action update

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   before_action :set_question!
+  before_action :set_answer!, except: :create
 
   def create
     @answer = @question.answers.build(answer_params)
@@ -13,9 +14,20 @@ class AnswersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @answer.update answer_params
+      flash[:success] = t '.success'
+      redirect_to question_path(@question, anchor: dom_id(@answer))
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    answer = @question.answers.find(params[:id])
-    answer.destroy
+    @answer.destroy
     flash[:success] = "Answer deleted!"
     redirect_to question_path(@question)
   end
@@ -24,6 +36,10 @@ class AnswersController < ApplicationController
 
   def set_question!
     @question = Question.find(params[:question_id])
+  end
+
+  def set_answer!
+    @answer = @question.answers.find(params[:id])
   end
 
   def answer_params
